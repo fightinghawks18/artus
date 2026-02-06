efa5f176613dfb9f0d895ec3c5afcada87c9c263
Tyler LePage <lepagetyler@hotmail.com>
2026-02-06 17:46:35 -0600

    Add new changes to Main
    - Main SHOULD now render something, but theres an issue. Will be fixed soon

--- Runtime/Source/Main.cpp
+++ Runtime/Source/Main.cpp
@@ -3,8 +3,23 @@
 #include <Artus/Core/Window.h>
 #include <Artus/Graphics/Device.h>
 #include <Artus/Graphics/Resources/CommandAllocator.h>
+#include <Artus/Graphics/Resources/GraphicsPipeline.h>
 
 using namespace Artus;
+
+struct Vertex {
+    std::array<float, 2> position;
+    std::array<float, 2> uvCoords;
+    std::array<float, 4> color;
+};
+
+static std::vector<Vertex> vertices = {
+    {{0.0f, 0.0f}, {0.0f, 0.0f}, {1.0f, 0.0f, 0.0f, 1.0f}},
+    {{1.0f, 1.0f}, {1.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 1.0f}},
+    {{0.0f, 1.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 1.0f, 1.0f}},
+};
+
+static std::vector<uint32_t> indices = {0, 1, 2};
 
 int main() {
     auto* window = new Core::Window();
@@ -12,6 +27,37 @@ int main() {
     auto* surface = new Graphics::Surface(*device, window);
     auto* cmdAllocator = new Graphics::CommandAllocator(*device);
     auto cmdEncoders = cmdAllocator->NewEncoders(3);
+
+    auto* vertexBuffer = new Graphics::Buffer(*device, sizeof(Vertex) * vertices.size(), Graphics::BufferUsage::Vertex);
+    auto* indexBuffer = new Graphics::Buffer(*device, sizeof(uint32_t) * indices.size(), Graphics::BufferUsage::Index);
+
+    vertexBuffer->Map(vertices.size() & sizeof(Vertex), 0, 0, vertices.data());
+    indexBuffer->Map(indices.size() * sizeof(uint32_t), 0, 0, indices.data());
+
+    auto* vertexShader = new Graphics::Shader(*device, "Shaders/VS_Default.spv");
+    auto* pixelShader = new Graphics::Shader(*device, "Shaders/PS_Default.spv");
+
+    auto* pipelineLayout = new Graphics::PipelineLayout(*device);
+
+    Graphics::GraphicsPipelineInputBinding vertexBinding = {.slot = 0, .stride = sizeof(Vertex)};
+    vertexBinding.attributes.push_back(
+        {.location = 0, .format = vk::Format::eR32G32Sfloat, .offset = offsetof(Vertex, position)});
+    vertexBinding.attributes.push_back(
+        {.location = 1, .format = vk::Format::eR32G32Sfloat, .offset = offsetof(Vertex, uvCoords)});
+    vertexBinding.attributes.push_back(
+        {.location = 2, .format = vk::Format::eR32G32B32A32Sfloat, .offset = offsetof(Vertex, color)});
+
+    Graphics::GraphicsPipelineDescriptor graphicsPipelineDescriptor = {};
+    graphicsPipelineDescriptor.bindings.push_back(vertexBinding);
+
+    graphicsPipelineDescriptor.vertexStage = vertexShader;
+    graphicsPipelineDescriptor.pixelStage = pixelShader;
+    graphicsPipelineDescriptor.pipelineLayout = pipelineLayout;
+    graphicsPipelineDescriptor.colorFormats = {surface->GetVulkanSurfaceFormat().format};
+    graphicsPipelineDescriptor.depthFormat = vk::Format::eD32SfloatS8Uint;
+    graphicsPipelineDescriptor.stencilFormat = vk::Format::eD32SfloatS8Uint;
+
+    auto* graphicsPipeline = new Graphics::GraphicsPipeline(*device, graphicsPipelineDescriptor);
 
     while (!window->IsClosing()) {
         Core::Window::Update();
@@ -38,13 +84,36 @@ int main() {
 
         const auto frameIndex = surface->GetFrameIndex();
 
+        vk::Viewport viewport = {};
+        viewport.setX(0)
+            .setY(0)
+            .setWidth(rect.extent.width)
+            .setHeight(rect.extent.height)
+            .setMinDepth(0)
+            .setMaxDepth(1.0f);
+
+        std::vector<vk::Rect2D> rects = {rect};
+        std::vector<vk::Viewport> viewports = {viewport};
+
         auto cmd = cmdEncoders[frameIndex];
-        cmd->Reset();
         cmd->Start();
 
         image->MakeRenderable(cmd->GetVulkanCommandBuffer());
 
         cmd->StartRendering(renderingInfo);
+
+        cmd->GetVulkanCommandBuffer().setCullMode(vk::CullModeFlagBits::eNone);
+        cmd->GetVulkanCommandBuffer().setDepthWriteEnable(false);
+        cmd->GetVulkanCommandBuffer().setDepthTestEnable(false);
+        cmd->GetVulkanCommandBuffer().setStencilTestEnable(false);
+        cmd->GetVulkanCommandBuffer().setViewportWithCount(viewports.size(), viewports.data());
+        cmd->GetVulkanCommandBuffer().setScissorWithCount(rects.size(), rects.data());
+
+        cmd->BindVertexBuffer(vertexBuffer);
+        cmd->BindIndexBuffer(indexBuffer);
+        cmd->BindGraphicsPipeline(graphicsPipeline);
+        cmd->DrawIndexed(indices.size(), 0);
+
         cmd->EndRendering();
 
         image->MakePresentable(cmd->GetVulkanCommandBuffer());
@@ -55,6 +124,14 @@ int main() {
     }
 
     device->GetVulkanDevice().waitIdle();
+
+    delete vertexShader;
+    delete pixelShader;
+
+    delete pipelineLayout;
+    delete graphicsPipeline;
+    delete vertexBuffer;
+    delete indexBuffer;
 
     delete cmdAllocator;
 
